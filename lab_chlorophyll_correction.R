5dea4c963bfca227ced383d79885655f47fc772d
B13rg <b11484@gmail.com>
2023-04-24 23:53:28 -0700

    Get it running

--- lab_chlorophyll_correction.R
+++ lab_chlorophyll_correction.R
@@ -1,9 +1,11 @@
 # Clark's lab data method changes adjustment function [https://green2.kingcounty.gov/ScienceLibrary/Document.aspx?ArticleID=324]
-
-lab_chlorophyll_correction <- function(Value,Parameter,Date){
+#
+# Checks for the 'Chlorophyll a' parameter
+#
+lab_chlorophyll_correction <- function(Value,Parameter='None',Date=as.Date("2999-07-01")){
   if(Parameter=='Chlorophyll a' & Date<as.Date("1996-07-01")){
     1.14*Value
   } else {
     Value
-  }  
+  }
 }
