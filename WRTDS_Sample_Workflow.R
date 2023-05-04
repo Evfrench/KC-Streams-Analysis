98c4bda5ab34aee7025cdb40684f9fb3bda225eb
evfrench <evfrench@uw.edu>
2023-05-03 18:28:41 -0700

    Added files with both DLM and WRTDS examples

---
+++ WRTDS_Sample_Workflow.R
@@ -0,0 +1,31 @@
+library(EGRET)
+
+############################
+# Gather discharge data:
+siteID <- "01491000" #Choptank River at Greensboro, MD
+startDate <- "" #Gets earliest date
+endDate <- "2011-09-30"
+# Gather sample data:
+parameter_cd<-"00631" #5 digit USGS code, this is the nitrate+nitrite as nitrogen 
+Sample <- readNWISSample(siteID,parameter_cd,startDate,endDate)
+#Gets earliest date from Sample record:
+#This is just one of many ways to assure the Daily record
+#spans the Sample record
+startDate <- min(as.character(Sample$Date)) 
+# Gather discharge data:
+Daily <- readNWISDaily(siteID,"00060",startDate,endDate)
+# Gather site and parameter information:
+
+# Here user must input some values for
+# the default (interactive=TRUE)
+INFO<- readNWISInfo(siteID,parameter_cd)
+INFO$shortName <- "Choptank River at Greensboro, MD"
+
+# Merge discharge with sample data:
+eList <- mergeReport(INFO, Daily, Sample)
+
+# Run WRTDS model:
+eList <- modelEstimation(eList)
+
+#eList:
+plotConcTimeDaily(eList)
