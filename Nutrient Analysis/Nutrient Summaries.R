e4282f158afe0a27ecf18f0f5ab766e505c60a35
evfrench <evfrench@uw.edu>
2023-07-18 18:59:24 -0700

    Renamed file

--- Nutrient Analysis/Nutrient Summaries.R
+++ Nutrient Analysis/Nutrient Summaries.R
@@ -3,3 +3,5 @@ library(data.table)
 
 bigTable <- fread('./data_cache/KC_WQ_Data')
 AnnualNO3_NO2 <- get_annual_median(bigTable, 'Nitrite_+_Nitrate_Nitrogen')
+
+Annual_OrthoP <- get_annual_median(bigTable, 'Orthophosphate_Phosphorus')
